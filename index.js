7886e1f085d75e08fd54a7509dee7b1f1944cd9e
Jose Vicente Giner <jvginer@gentooxativa.com>
2020-10-22 13:17:27 +0200

    fix: fix scm generation when there is no git repository

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ const gitRemote = require('remote-origin-url');
 
 module.exports = extra => {
     const { name, version } = pkg;
-    const scm = {
+    let scm = {};
+
+    if( fs.existsSync(path.join(process.cwd(), '.git'))){
+      scm = {
         remote: gitRemote.sync(),
         branch: gitCommit.branch(),
         commit: gitCommit.long()
+      }
     }
-    const timestamp = new Date().toISOString();
 
+    const timestamp = new Date().toISOString();
     const manifest = { name, version, scm, timestamp, ...extra };
     fs.writeFileSync('manifest.json', JSON.stringify(manifest, null, 2) + '\n', 'utf-8')
 }
