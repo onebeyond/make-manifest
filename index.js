b485c62563a2a14e005dd8e81af694618e21cfaf
Jose Vicente Giner Sanchez <gentooxativa@gentooxativa.com>
2020-10-22 10:31:28 +0200

    feat: upgraded package versions and es6 refactor (#5)
    Co-authored-by: Jose V Giner <jose.giner@ara-tech.es>

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-var fs = require('fs')
-var path = require('path')
-var pkg = require(path.join(process.cwd(), 'package.json'))
-var merge = require('lodash.merge')
-var gitCommit = require('git-rev-sync');
-var gitRemote = require('remote-origin-url');
+const fs = require('fs')
+const path = require('path')
+const pkg = require(path.join(process.cwd(), 'package.json'))
+const gitCommit = require('git-rev-sync');
+const gitRemote = require('remote-origin-url');
 
-module.exports = function(extra) {
-    var manifest = merge({
-        name: pkg.name,
-        version: pkg.version,
-        timestamp: new Date().toISOString(),
-        scm: {
-            remote: gitRemote.sync(),
-            branch: gitCommit.branch(),
-            commit: gitCommit.long()
-        }
-    }, extra)
+module.exports = extra => {
+    const { name, version } = pkg;
+    const scm = {
+        remote: gitRemote.sync(),
+        branch: gitCommit.branch(),
+        commit: gitCommit.long()
+    }
+    const timestamp = new Date().toISOString();
+
+    const manifest = { name, version, scm, timestamp, ...extra };
     fs.writeFileSync('manifest.json', JSON.stringify(manifest, null, 2) + '\n', 'utf-8')
 }
