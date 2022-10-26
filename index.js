3525b52ca45fdb26b028332632491c7533c04ddc
Íñigo Marquínez Prado <inigo.marquinez@guidesmiths.com>
2022-10-26 10:47:16 +0200

    chore: updated commander to latest major version

--- index.js
+++ index.js
@@ -5,19 +5,20 @@ const gitCommit = require('git-rev-sync');
 const gitRemote = require('remote-origin-url');
 
 module.exports = extra => {
-    const { name, version } = pkg;
-    let scm = {};
+  const { name, version } = pkg;
+  let scm = {};
 
-    // Without commits, .git/packed-refs does not exists
-    if( fs.existsSync(path.join(process.cwd(), '.git', 'packed-refs'))){
-      scm = {
-        remote: gitRemote.sync(),
-        branch: gitCommit.branch(),
-        commit: gitCommit.long()
-      }
+  // Without commits, .git/packed-refs does not exists
+  if (fs.existsSync(path.join(process.cwd(), '.git', 'packed-refs'))) {
+    scm = {
+      remote: gitRemote.sync(),
+      branch: gitCommit.branch(),
+      commit: gitCommit.long()
     }
+  }
 
-    const timestamp = new Date().toISOString();
-    const manifest = { name, version, scm, timestamp, ...extra };
-    fs.writeFileSync('manifest.json', `${JSON.stringify(manifest, null, 2)}\n`, 'utf-8')
+  const timestamp = new Date().toISOString();
+  const manifest = { name, version, scm, timestamp, ...extra };
+
+  fs.writeFileSync('manifest.json', `${JSON.stringify(manifest, null, 2)}\n`, 'utf-8');
 }
